fe680c2b6bf7f94937d93b8f5f81ae72496e209a
The Untitled <nguyenminhcuong2000@gmail.com>
2022-09-04 17:27:20 +0700

    #2022/09/04 cuong - create routes for crud node

--- starter/app.js
+++ starter/app.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const express = require("express");
+const { route } = require("./routes/tasks");
 const app = express();
+const tasks = require("./routes/tasks");
+
+// Middleware
+app.use(express.json())
 
 // Routes
-app.get('/hello', (req, res) =>{
-    res.send('Task Manager App')
-})
-const port = 3000
-app.listen(port, console.log(`server is listening on port ${port} ...`))
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+/*
+app.get('/api/v1/tasks')
+app.post('/api/v1/tasks')
+app.get('/api/v1/tasks/:id')
+app.path('/api/v1/tasks/:id')
+app.delete('/api/v1/tasks/:id')
+*/
+
+const port = 3000;
+app.listen(port, console.log(`server is listening on port ${port} ...`));
